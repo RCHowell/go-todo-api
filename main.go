754e16e658232c30367c4fb696c5ee077becd53b
R. C. Howell <rhowell2@nd.edu>
2018-03-08 21:48:52 -0500

    Updated to use a map because it makes updating and deleting easier. Also, I cleaned up some code and comments

--- main.go
+++ main.go
@@ -6,95 +6,80 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin" // The framework
 	"net/http" // Used for http.Status_
-	"time" // Used for generating id's
-	"strconv" // Used for converting strings to ints
+	"strconv"
+	"time"
+	// Used for generating id's
+	// Used for converting strings to ints
+
+	"github.com/gin-gonic/gin" // The framework
 )
 
-// Model for a TODO
-type TODO struct {
-	Id int `json:"id"`
-	Done bool `json:"done"`
+// Define how a toDo should be represented
+type toDo struct {
+	ID   int64  `json:"id"`
+	Done bool   `json:"done"`
 	Text string `json:"text"`
 }
 
-// In-memory data store
-// I chose to use a slice instead of a map
-// because there's no built-in map -> slice method
-// and O(n) lookup on update and delete is reasonable given
-// the small data size
-var TODOS []TODO
+// ToDos is our in memory data store and using a map
+// gives us O(1) for Create,Update, and Delete
+var toDos map[int64]*toDo
 
-// Create a new TODO
-func create (c *gin.Context) {
+// Create a new toDo
+func createToDo(c *gin.Context) {
 	// Get text from url parameter
 	text := c.Query("text")
-	// Append a new TODO to the list
-	TODOS = append(TODOS, TODO{
-		Id: time.Now().Nanosecond(),
+	// Use nanoseconds so that toDo.ID is a ~hopefully~ unique value
+	id := time.Now().UnixNano()
+	// Add the address of our new toDo to the data store
+	toDos[id] = &toDo{
+		ID:   id,
 		Done: false,
 		Text: text,
-	})
+	}
 	// Return with success
 	c.JSON(http.StatusCreated, "Created")
 }
 
-// Return all TODO's
-func read (c *gin.Context) {
-	// We hold this method to be self-evident
-	c.JSON(http.StatusOK, TODOS)
+// Respond with a slice of toDos
+// Constructing the slice runs in O(n)
+func readToDos(c *gin.Context) {
+	toDosSlice := make([]toDo, len(toDos))
+	i := 0
+	for _, val := range toDos {
+		toDosSlice[i] = *val
+		i++
+	}
+	c.JSON(http.StatusOK, toDosSlice)
 }
 
-// Update a single TODO
-func update (c *gin.Context) {
+// Update a single toDo
+func updateToDo(c *gin.Context) {
 	// Get the TODO id from url parameter
 	id, _ := strconv.Atoi(c.Query("id"))
-	found := false
-	// Lookup the TODO to update: O(n)
-	for i := range TODOS {
-		if (TODOS[i].Id == id) {
-			// Update TODO status
-			found = true
-			TODOS[i].Done = !TODOS[i].Done
-		}
-	}
-	// Return with success if TODO was found
-	if (found) {
-		c.JSON(http.StatusOK, "Updated")
-	} else {
-		c.JSON(http.StatusBadRequest, "Id not found")
-	}
+	toDos[int64(id)].Done = !toDos[int64(id)].Done
+	c.JSON(http.StatusOK, "Updated")
 }
 
-// Delete a TODO
-func delete (c *gin.Context) {
+// Delete a toDo
+func deleteToDo(c *gin.Context) {
 	id, _ := strconv.Atoi(c.Query("id"))
-	found := false
-	// Lookup the TODO to delete: O(n)
-	for i := range TODOS {
-		if (TODOS[i].Id == id) {
-			found = true
-			// Delete TODO from the slice
-			TODOS = append(TODOS[:i], TODOS[i+1:]...)
-		}
-	}
-	// Return with success if TODO was found
-	if (found) {
-		c.JSON(http.StatusOK, "Deleted")
-	} else {
-		c.JSON(http.StatusBadRequest, "Id not found")
-	}
+	// Delete takes no action if the key is not present
+	delete(toDos, int64(id))
+	c.JSON(http.StatusOK, "Deleted")
 }
 
 func main() {
 	// Initialize gin with default middleware
 	r := gin.Default()
-	// Attach routes to REST methods and CRUD actions on our TODO's
-	r.POST("/", create)
-	r.GET("/", read)
-	r.PUT("/", update)
-	r.DELETE("/", delete)
+	// Initialize our data store
+	toDos = make(map[int64]*toDo)
+	// Attach routes to REST methods and CRUD actions on our ToDo's
+	r.POST("/", createToDo)
+	r.GET("/", readToDos)
+	r.PUT("/", updateToDo)
+	r.DELETE("/", deleteToDo)
 	// Listen on port 1337
 	r.Run(":1337")
 }
